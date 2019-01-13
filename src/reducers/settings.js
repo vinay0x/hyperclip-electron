565618334b58c57c6af2c62ea424db766ad3166b
Vinay <vinay@surveysparrow.com>
2019-01-13 17:38:06 +0530

    Changed shortcut as it was conflicting

--- src/reducers/settings.js
+++ src/reducers/settings.js
@@ -1,12 +1,19 @@
 const SET_DARKMODE = 'settings/setDarkMode'
+const SET_SHOW_SETTINGS = 'settings/setShowSettings'
 
 export const setDarkMode = (payload) => ({
   type: SET_DARKMODE,
   payload
 })
 
+export const setFirstLoadFalse = (payload) => ({
+  type: SET_SHOW_SETTINGS,
+  payload
+})
+
 const initialState = {
-  darkMode: false
+  darkMode: false,
+  showSettings: true
 }
 
 export default (state = initialState, action) => {
@@ -15,6 +22,11 @@ export default (state = initialState, action) => {
       return {
         ...state,
         darkMode: action.payload
+      }
+    case SET_SHOW_SETTINGS:
+      return {
+        ...state,
+        showSettings: action.payload
       }
     default:
       return state
